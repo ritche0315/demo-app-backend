6c4c1f0711fb725cf66da85c65068a210fc7f090
ritche0315 <ritche2000@gmail.com>
2023-08-17 01:20:06 +0800

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
-
+app.use(cors());
 app.get('/',(req,res,next)=>{
     res.status(200).json({message: "get resource"})
 })
